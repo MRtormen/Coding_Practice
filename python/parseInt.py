cf2f0c916d4340e1fb57a0172c27eb7aeec739d9
MRtormen <you@example.com>
2023-02-15 21:14:07 +0000

    some comments

--- python/parseInt.py
+++ python/parseInt.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
+# This is a kata from Codewars.
+# The exercise can be found at https://www.codewars.com/kata/525c7c5ab6aecef16e0001a5
+
 import sys 
 
 def parse_int(string):
     string = string.replace("-", " ")
-    list = string.split()
+    l = string.split()
     small_numbers = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6,\
                      "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13,\
                      "fourteen": 14, "fifteen": 15, "sixteen": 16, "seventeen": 17, "eighteen": 18, "nineteen": 19,\
@@ -11,25 +15,25 @@ def parse_int(string):
                      "ninety": 90}
     big_numbers = {"hundred": 100, "thousand": 1000, "million": 1000000}
     total = 0
-    n = 0
-    for word in list:
-        if word == "and":
+    temp = 0 # Used to save temporary total value
+    for word in l:
+        if word == "and": # Detect and ignore 'and'
             continue
         if word in small_numbers:
-            n += small_numbers[word]
+            temp += small_numbers[word]
         elif word in big_numbers:
-            x = n * big_numbers[word]
+            x = temp * big_numbers[word]
             if total < x:
                 total = total * big_numbers[word] + x
-            elif n == 0:
+            elif temp == 0:
                 total = total * big_numbers[word]
             else:
                 total += x
-            n = 0
+            temp = 0 # Reseting temporary value
         else: # Word not in dictionary or exeeds max value
             print(f"Can't parse {word}, check spelling (max value is 1 million).")
             raise SystemExit(2)
-    total += n
+    total += temp
     return total
 
 def main():
